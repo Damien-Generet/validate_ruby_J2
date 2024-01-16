2c0a75891462454f7affe4c0153213811af2350d
Damien-Generet <damiengenret@outlook.com>
2024-01-16 08:53:12 +0100

    finish vfalidate exo

--- exo_15.rb
+++ exo_15.rb
@@ -1,6 +1,11 @@
-puts "Donnes un nombre entre 1 et 25"
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 number = gets.chomp.to_i
 pyramid = "#"
+
+while number < 5 or number > 25 
+    puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?" 
+    number = gets.chomp.to_i
+end
 
 number.times do |i|
     puts pyramid * (i + 1)
--- exo_17.rb
+++ exo_17.rb
@@ -1,7 +1,12 @@
-puts "Donnes un nombre entre 1 et 25"
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 number = gets.chomp.to_i
 pyramid = "#"
 
+
+while number < 5 or number > 25 
+    puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?" 
+    number = gets.chomp.to_i
+end
 
 number.times do |i|
     dieses = "#" * (i + 1)
